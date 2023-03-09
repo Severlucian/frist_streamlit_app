09957661b6f72d23f7474e07527f99f0ad059c98
Severlucian <114876775+Severlucian@users.noreply.github.com>
2023-03-09 13:44:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ streamlit.header(' 🥣 Breakfast Menu')
 streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑Hard-Boiled Free-Range Egg')
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+
 
  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
